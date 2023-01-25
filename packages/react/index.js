ce5bcc6d0ed15fa9ac3d64edcd7ed279c4056b50
Neeraj Lagwankar <neeraj@codebuddy.co>
2023-01-25 11:13:38 +0530

    feat: rename packages, add prettier

---
+++ packages/react/index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    '@codebuddy/eslint-config-base',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 0,
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['vite.config.js'] }],
+  },
+};
